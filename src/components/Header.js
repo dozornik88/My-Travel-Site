0b3bd8a5f7f6f11753d7ffcdbd61138c1b082d93
Sergejs Dementjevs <dozornik88@gmail.com>
2020-12-11 20:29:28 +0100

    working mobile navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,50 @@
 import { Link } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Header.css";
 
 import Button from "./Button";
-import Bars from "./Bars";
 
 const Header = () => {
+	const [click, setClick] = useState(false);
+	const handleClick = () => setClick(!click);
+	const closeMobileMenu = () => setClick(false);
+
 	return (
 		<>
-			<div className="nav-container">
-				<Link to="/" className="navlink">
-					WorldTravel
-				</Link>
-				<Bars />
-				<div className="navmenu"></div>
-				<div className="navbtn">
-					<Link to="/pagetwo">
-						<Button />{" "}
+			<nav className="navbar">
+				<div className="navbar-container">
+					<Link to="/" className="navbar-logo">
+						Travel Blog
 					</Link>
+					<div className="menu-icon" onClick={handleClick}>
+						<i className={click ? "fas fa-times" : "fas fa-bars"} />
+					</div>
+
+					<ul className={click ? "nav-menu active" : "nav-menu"}>
+						<li className="nav-item">
+							<Link to="/" className="nav-links" onClick={closeMobileMenu}>
+								Home
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link to="/services" className="nav-links">
+								Services
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link to="/products" className="nav-links">
+								Products
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link to="/sign-up" className="nav-links-mobile">
+								Sign up
+							</Link>
+						</li>
+					</ul>
+					<Button />
 				</div>
-			</div>
+			</nav>
 		</>
 	);
 };
